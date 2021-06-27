e7052a208ed709e246d2516873a8fc861abe3914
Kunj Dhami <kunjdhami26@gmail.com>
2021-06-27 13:35:48 +0530

    server side completed and moving to db

--- mean-project/backend/app.js
+++ mean-project/backend/app.js
@@ -1,6 +1,33 @@
+// express app
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express(); //return express app which is a big chainof middlewares we apply to incoming requests
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  )
+  next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log('buhaha ',post);
+  res.status(201).json({
+    message: 'Post added sccssflly'
+  });
+});
 
 app.use('/api/posts', (req, res, next) => {
   const posts = [
--- mean-project/src/app/app.module.ts
+++ mean-project/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatExpansionModule} from '@angular/material/expansion';
-
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -32,7 +32,8 @@ import { CommonModule } from '@angular/common';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
