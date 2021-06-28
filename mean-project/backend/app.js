6504a029daa00026a4aa66d9fbb2b9a3b3fcb63b
Kunj Dhami <kunjdhami26@gmail.com>
2021-06-28 11:47:26 +0530

    done with db connection

--- mean-project/backend/app.js
+++ mean-project/backend/app.js
@@ -2,8 +2,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/posts');
 
 const app = express(); //return express app which is a big chainof middlewares we apply to incoming requests
+
+mongoose.connect("mongodb+srv://max:L7zBEbbN7aAkpsU@cluster0.ucekb.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to db');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -22,30 +33,39 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log('buhaha ',post);
-  res.status(201).json({
-    message: 'Post added sccssflly'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    console.log('from backend on add new post', createdPost);
+    res.status(201).json({
+      message: 'Post added sccssflly',
+      postId: createdPost._id
+    });
+  }); //insert to db
+  console.log('came from backend ', post);
+});
+
+app.get('/api/posts', (req, res, next) => {
+  Post.find().then(documents => {
+    console.log('documents from app js', documents);
+    res.status(200).json({
+      message: 'posts fetched successfully',
+      posts: documents
+    });
   });
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: "ugy1i21",
-      title: "First server post",
-      content: "coming from server"
-    },
-    {
-      id: "sadf3423",
-      title: "2nd server post",
-      content: "coming from server again"
-    }
-  ];
-  res.status(200).json({
-    message: 'posts fetched successfully',
-    posts: posts
-  });
+app.delete('/api/posts/:id', (req, res, next) => {
+  console.log('id came from frontend and this is from backend ',req.params.id);
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log('result from deletion in backend ',result);
+    res.status(200).json({ message: 'Post Deleted' });
+  })
 });
 
 module.exports = app;
+
+
+//mongo pass: L7zBEbbN7aAkpsU
